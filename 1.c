1e54782d00d1a66d8fed1386c20e2e362af56ceb
Joel <JoelVIctor1746@gmail.com>
2018-03-05 22:01:11 -0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main(){
+    int A;
+    int B;
+    printf("Digite 2 números separados por um espaço!\n");
+    scanf("%d %d", &A, &B);
+    int X = A + B;
+    printf("X = %d", X);
+        return 0;
+}
