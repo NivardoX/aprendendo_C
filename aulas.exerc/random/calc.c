45e0a0bd5921c5ff7ebfebd102741b87b79b7750
Joel <JoelVIctor1746@gmail.com>
2018-03-16 20:18:33 -0300

    Create calc.c

---
+++ aulas.exerc/random/calc.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(){
+
+	char opc;
+	printf("Selecione uma das operacoes : \nS - Soma\nD - Diferenca\nP - produto\nQ - Divisao\n");
+	scanf("%c", &opc);
+	
+	int x;
+	int y;
+	
+	printf("Digite os numeros a serem operados!\n");	
+	scanf("%d %d", &x, &y);
+
+	if(opc == 'q' && y == 0) {
+		printf("operacao invalida\n");
+		}else {
+		
+		int soma = x + y;
+		int sub = x - y;
+		int prod = x * y;
+		int div = x / y;
+				
+		switch(opc){
+			case 's':
+			printf("O resultado da soma eh %d\n", soma);
+			break;
+			case 'd':
+			printf("O resultado da subtracao eh %d\n", sub);
+			break;
+			case 'p':
+			printf("O resultado da multiplicacao eh %d\n", prod);
+			break;
+			case 'q':
+			printf("O resultado da divisao eh %d\n", div);
+			break;
+			default:
+			printf("Invalido!");
+		}
+	}
+
+
+
+	printf("Opcao : %c\nNumeros %d e %d\n", opc, x, y); //teste
+
+	return 0;
+}
