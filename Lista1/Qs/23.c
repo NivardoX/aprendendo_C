9c6880a380589c20b4f8673e1835fa8d9695b95f
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-24 23:04:14 +0000

    Update 23.c

--- Lista1/Qs/23.c
+++ Lista1/Qs/23.c
@@ -6,7 +6,7 @@ int main(){
 
 	printf("Digite um numero inteiro e um numero flutuante para saber o produto entre os mesmos!\n");
 	scanf("%d %f", &n, &f);
-	printf("O produto entre os dois valores eh: %.2f\n", f * n);	
+	printf("O produto do numero %.2f e %d eh: %.2f\n", f, n, f * n);	
 
 	return 0;
 }
