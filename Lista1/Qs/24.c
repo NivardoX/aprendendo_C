3128c88b400112a6e080ffc6903505cd3d43b2b7
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-24 23:02:09 +0000

    Update 24.c

--- Lista1/Qs/24.c
+++ Lista1/Qs/24.c
@@ -5,7 +5,7 @@ int main(){
 
 	printf("Digite dois numeros inteiros para saber a media entre os mesmos!\n");
 	scanf("%d %d", &n1, &n2);
-	printf("O media entre os dois valores eh: %.2f\n", ((float)n1+n2)/2);	
+	printf("O media entre os dois valores eh: %.1f\n", ((float)n1+n2)/2);	
 
 	return 0;
 }
