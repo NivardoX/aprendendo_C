5b7cfc4eb8c39b4b77619a3bb25af062ba1b53da
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-24 22:40:14 +0000

    Create 20.c

---
+++ Lista1/Qs/20.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+
+	printf("Camisa regata\t\t-\t50%% de desconto\t\t-\tR$ 19,90\nSandalia Masculina\t-\t17%% de desconto\t\t-\tR$ 30,00\nTenis\t\t\t-\t77%% de desconto\t\t-\tR$ 33,00\n");	
+
+	return 0;
+}
