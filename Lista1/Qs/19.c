0b6e5b9b87788c1b806908f2b935d569e8f1cb6d
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-24 22:36:38 +0000

    Create 19.c

---
+++ Lista1/Qs/19.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+
+	printf("Instituicao:\tIFCE - Instituto Federal de Educacao, ciencia e tecnologia do Ceara\nCurso:\t\tEngenharia de computacao\nSemestre:\t2017.1\n");	
+
+	return 0;
+}
