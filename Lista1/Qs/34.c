af213912d4a1f5be495130ab2a1a001dd8174433
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-26 01:06:49 +0000

    Create 34.c

---
+++ Lista1/Qs/34.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(){
+
+	int a,b,c,aux;
+	
+	printf("Digite o primeiro numero!\n");
+	scanf("%d", &a); 
+	printf("Digite o segundo numero!\n");
+	scanf("%d", &b); 
+	printf("Digite o terceiro numero!\n");
+	scanf("%d", &c); 
+
+	if(a > b){
+		aux = a;
+		a = b;
+		b = aux;
+	}
+	if(a > c){
+		aux = a;
+		a = c;
+		c = aux;
+	}
+	if(b > c){
+		aux = b;
+		b = c;
+		c = aux;
+	}
+
+	printf("Os tres valores em ordem crescente sao: %d, %d e %d\n",a,b,c);
+
+	return 0;
+}
