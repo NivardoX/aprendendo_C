3dcf8c4f8bf30133f0952285057587c0b358872d
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-29 02:05:11 +0000

    Update at3.c

--- aulas.exerc/28/at3.c
+++ aulas.exerc/28/at3.c
@@ -5,9 +5,9 @@ int main(){
 	int flag = 0,y,x,xPosic,xCount,xDesejado;
 
 	for(y = 0; y <= 9; y++){
-		printf("Digit o %d numero\n", y);
+		printf("Digit o %d numero\n", y+1);
 		scanf("%d", &vt[y]);
-}
+	}
 
 	printf("Digite o valor a ser buscado\n");
 	scanf("%d", &xDesejado);
@@ -18,26 +18,24 @@ int main(){
 			xPosic = x;
 			flag = 1;
 		}else{
-//			if(xCount == 10 && flag ==0){
-//				printf("Este valor nao foi encontrado\n");			
-//				return 0;			
-//			}else{
-				x++;
-//			}
-	}
-}
+			x++;
+		}
+		xCount++;
+	}	
 	
-	if(xCount == 10 && flag == 0){
-		return 0;
+
+	if(flag == 1){
+		printf("O valor desejado era %d.\nEle foi encontrado na %d posicao\n", xDesejado, xPosic+1);
 	}else{
-		printf("O valor desejado era %d.\nEle foi encontrado na %d posicao\n", xDesejado, xPosic++);
-
-}
-
-
+		printf("Valor nao encontrado\n");
+	}
+	
+	printf("valores inseridos : ");
 	for(y = 0; y <= 9; y++){
 		printf("%d, ", vt[y]);
-}
+	}
+	
 
 	return 0;
+
 }
