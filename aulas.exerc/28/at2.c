53aeb9515486d5a03e32771ee85018a6a3424d73
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-03-29 02:23:45 +0000

    Update at2.c

--- aulas.exerc/28/at2.c
+++ aulas.exerc/28/at2.c
@@ -1,31 +1,45 @@
 #include <stdio.h>
 int main(){
 
-	int vt[10];// = {5,3,7,2,9,0,1,4,6,8};
-	int x, x1, aux, y;
+	int vt[10];
+	int x, x1, aux, y,soma,media;
 
 	
 	for(y = 0; y <= 9; y++){
-		printf("digite vt %d\n", y);
+		printf("digite vt %d\n", y+1);
 		scanf("%d", &vt[y]);
-		}
+	}
 	
 	x = 0;
-	while(x < 9){
+	while(x <= 9){		//funcao de ordem, em funcao de valor x
 		x1 = x + 1;
-		if(vt[x] >= vt[x1]){			
-			aux = vt[x];
-			vt[x] = vt[x1];
-			vt[x1] = aux;
-			x1++;
+		while(x1 <= 9){	//funcao de ordem, em funcao de valor x1
+			if(vt[x] >= vt[x1]){	
+				aux = vt[x];
+				vt[x] = vt[x1];
+				vt[x1] = aux;
+			}
+		x1++;
 		}
-		x++;
+	x++;		
 	}
 	
 
 	for(y = 0; y <= 9; y++){
 		printf("%d, ", vt[y]);
 	}
+
+	soma = 0;			//funcao p/ soma
+	for(y = 0; y <= 9; y++){
+		soma += vt[y];
+}
+
+	media = soma/10;
+
+	printf("\nMaior valor : %d\n",vt[9]);
+	printf("Menor valor : %d\n",vt[0]);
+	printf("Media : %d\n", media);
+	printf("Soma : %d\n", soma);
 	
 	return 0;
 }
