41c19a43f4de6148fcbb0c5b7108581abf10603f
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-19 17:32:50 +0000

    Update 1259.c

--- aulas.exerc/URI/1259.c
+++ aulas.exerc/URI/1259.c
@@ -1,25 +1,31 @@
 #include <stdio.h> //unfinished
 int main(){
 
+	int vt[10000], i, n,aux;
 
-
-	while(scanf("%d", n) != EOF){
+	while(scanf("%d", &n) != EOF){
 	
 		for(i = 0; i < n; i++){
-			scanf("n", vt[i])
-			v[i] = n;		
+			scanf("%d", &vt[i]);
 	
 			if(vt[i]%2 == 0 && i != 0){ //verificar se é par ou 1 posicao
 				if(vt[i-1]%2 == 1 | vt[i-1] > vt[i]){ //verificar se anterior é impar ou maior
-					vt[i] = aux;
+					aux = vt[i]; //troca de variaveis
 					vt[i] = vt[i-1];
 					vt[i-1] = aux;
 				}
+			}else if(vt[i]%2 == 1 && i != 0){ //verificar se é impar ou 1 posicao
+			    if(vt[i-1]%2 == 1 && vt[i-1] < vt[i]){ //verificar se anterior é impar e se é menor
+			        aux = vt[i]; //troca de variaveis
+					vt[i] = vt[i-1];
+					vt[i-1] = aux;
+			    }
 			}
 		}
 		for(i = 0 ; i <  n; i++){
 			printf("%d, ", vt[i]);
 		}
+		printf("\n");
 	}
 
 	return 0;
