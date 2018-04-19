de6a1b1907e792d665430daccc30fb371e8cc81f
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-19 17:42:31 +0000

    Update 1259.c

--- aulas.exerc/URI/1259.c
+++ aulas.exerc/URI/1259.c
@@ -8,21 +8,19 @@ int main(){
 		for(i = 0; i < n; i++){
 			scanf("%d", &vt[i]);
 	
-			if(vt[i]%2 == 0 && i != 0){ //verificar se é par ou 1 posicao
-				if(vt[i-1]%2 == 1 | vt[i-1] > vt[i]){ //verificar se anterior é impar ou maior
-					aux = vt[i]; //troca de variaveis
-					vt[i] = vt[i-1];
-					vt[i-1] = aux;
-				}
-			}else if(vt[i]%2 == 1 && i != 0){ //verificar se é impar ou 1 posicao
-			    if(vt[i-1]%2 == 1 && vt[i-1] < vt[i]){ //verificar se anterior é impar e se é menor
-			        aux = vt[i]; //troca de variaveis
-					vt[i] = vt[i-1];
-					vt[i-1] = aux;
-			    }
+	        while(i != 0 && vt[i]% 2 == 0 &&(vt[i-1]%2 == 1 | vt[i-1] > vt[i])){ //verificar se é par ou 1 posicao e se anterior é impar 
+				aux = vt[i]; //troca de variaveis
+				vt[i] = vt[i-1];
+				vt[i-1] = aux;
 			}
+			while(i != 0 && vt[i]% 2 == 1 &&(vt[i-1]%2 == 1 & vt[i-1] > vt[i])){ // verificar se é impar ou 1 posicao e ordem
+		        aux = vt[i]; //troca de variaveis
+				vt[i] = vt[i-1];
+				vt[i-1] = aux;
+		    }
 		}
-		for(i = 0 ; i <  n; i++){
+		
+		for(i = 0 ; i < n; i++){
 			printf("%d, ", vt[i]);
 		}
 		printf("\n");
