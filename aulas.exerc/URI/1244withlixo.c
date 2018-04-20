184e3aafd8589f485bbf081dfa45e1edf0f6f9e5
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-20 04:19:25 +0000

    Update 1244withlixo.c

--- aulas.exerc/URI/1244withlixo.c
+++ aulas.exerc/URI/1244withlixo.c
@@ -28,26 +28,9 @@ int main(){
 				}
 			i++;
 		}
-
-
-		//test
-		printf("%s\n", str);
-
-		for(i = 0; i < j; i++){
-			printf("%d, ", vtI[i]);
-		}
-		printf("\n");
-		for(i = 0; i < j; i++){
-			printf("%d, ", vtT[i]);
-		}
-		printf("\n\n");
-
-
-
-
 		for(k = 0; k < p; k++){//ordernar vetT, e realizar msm ordenacao em vetI
 			for(i = 0; i < p; i++){
-				if(vtT[i] > vtT[i+1]){//ordem de tamanhos
+				if(vtT[i] < vtT[i+1]){//ordem de tamanhos
 					aux = vtT[i];//troca de vtT
 					vtT[i] = vtT[i+1];
 					vtT[i+1] = aux;
@@ -58,42 +41,16 @@ int main(){
 				}
 			}
 		}
-
-	printf("\n\n");
-	for(i = 0; i < 4; i++){
-		k = vtI[i] - 1;
-		while(k < vtT[i]){
-			if(str[k] != ' '){
-				printf("%c", str[k]);		
+					
+		for(i = 0; i < p; i++){
+			k = vtI[i] - 1;
+			while(str[k] != ' ' && str[k] != '\0'){
+				printf("%c", str[k]);
+				k++;
 			}
-			k++;
+			printf(" ");
 		}
-		printf(" ");
+		printf("\n");
 	}
-	printf("\n\n");
-
-
-	
-	//test
-	printf("%s\n", str);
-
-	for(i = 0; i < j; i++){
-		printf("%d, ", vtI[i]);
-	}
-	printf("\n");
-	for(i = 0; i < j; i++){
-		printf("%d, ", vtT[i]);
-	}
-	printf("\n");
-	printf("%c\n", str[0]);
-
-
-
-	}	
-
-
-
- 
-
    return 0;
 }
