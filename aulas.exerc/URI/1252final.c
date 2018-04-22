629a0c5e21e4dc1bd5a7c62e665007016ddc0468
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-22 20:06:43 +0000

    Update 1252final.c

--- aulas.exerc/URI/1252final.c
+++ aulas.exerc/URI/1252final.c
@@ -13,168 +13,172 @@ int main(){
 	int i,n,m,j;
 	
 
-	while(scanf("%d %d",&n,&m), n!=0 && m!=0){
-		j = n;
-	
-		posV02 = 0;
-		posV01 = 0;
-		posV0 = 0;
-		posV1 = 0;
-		posV2 = 0;
+	while(scanf("%d %d",&n,&m)){
+		if(n == 0 && m == 0){
+			printf("0 0\n");
+			break;
+		}else{
+			j = n;
 		
-		while(j--){
+			posV02 = 0;
+			posV01 = 0;
+			posV0 = 0;
+			posV1 = 0;
+			posV2 = 0;
+			
+			while(j--){
 
-			scanf("%i", &v);
-			
-			if(v%m == 0){ // modulo 0; //correto
-				if(posV0 == 0){
-					vt0[0] = v;		
-				}else{
-					i = posV0 - 1;
-					while(i >= 0){
-						//sort
-						if(abs(vt0[i])%2 == 0 && abs(v)%2 == 1){
-							vt0[i+1] = vt0[i];						
-							i--;
-						}else if(abs(vt0[i])%2 == 0 && abs(v)%2 == 0 && vt0[i] > v){
-							vt0[i+1] = vt0[i];						
-							i--;
-						}else if(abs(vt0[i])%2 == 1 && abs(v)%2 == 1 && vt0[i] < v){
-							vt0[i+1] = vt0[i];						
-							i--;
-						}else{
-							break;
+				scanf("%i", &v);
+				
+				if(v%m == 0){ // modulo 0; //correto
+					if(posV0 == 0){
+						vt0[0] = v;		
+					}else{
+						i = posV0 - 1;
+						while(i >= 0){
+							//sort
+							if(abs(vt0[i])%2 == 0 && abs(v)%2 == 1){
+								vt0[i+1] = vt0[i];						
+								i--;
+							}else if(abs(vt0[i])%2 == 0 && abs(v)%2 == 0 && vt0[i] > v){
+								vt0[i+1] = vt0[i];						
+								i--;
+							}else if(abs(vt0[i])%2 == 1 && abs(v)%2 == 1 && vt0[i] < v){
+								vt0[i+1] = vt0[i];						
+								i--;
+							}else{
+								break;
+							}
 						}
+						vt0[i+1] = v;	
 					}
-					vt0[i+1] = v;	
-				}
-				posV0++;
+					posV0++;
 
-			}else if(v%m == 1){ // modulo 1; //correto
-				if(posV1 == 0){
-					vt1[0] = v;		
-				}else{
-					i = posV1 - 1;
-					while(i >= 0){
-						//sort
-						if(abs(vt1[i])%2 == 0 && abs(v)%2 == 1){
-							vt1[i+1] = vt1[i];						
-							i--;
-						}else if(abs(vt1[i])%2 == 0 && abs(v)%2 == 0 && vt1[i] > v){
-							vt1[i+1] = vt1[i];						
-							i--;
-						}else if(abs(vt1[i])%2 == 1 && abs(v)%2 == 1 && vt1[i] < v){
-							vt1[i+1] = vt1[i];						
-							i--;
-						}else{
-							break;
+				}else if(v%m == 1){ // modulo 1; //correto
+					if(posV1 == 0){
+						vt1[0] = v;		
+					}else{
+						i = posV1 - 1;
+						while(i >= 0){
+							//sort
+							if(abs(vt1[i])%2 == 0 && abs(v)%2 == 1){
+								vt1[i+1] = vt1[i];						
+								i--;
+							}else if(abs(vt1[i])%2 == 0 && abs(v)%2 == 0 && vt1[i] > v){
+								vt1[i+1] = vt1[i];						
+								i--;
+							}else if(abs(vt1[i])%2 == 1 && abs(v)%2 == 1 && vt1[i] < v){
+								vt1[i+1] = vt1[i];						
+								i--;
+							}else{
+								break;
+							}
 						}
+						vt1[i+1] = v;	
 					}
-					vt1[i+1] = v;	
-				}
-				posV1++;
-			
-			}else if(v%m > 1){ // modulo maior q 1; //correto
-				if(posV2 == 0){
-					vt2[0] = v;		
-				}else{
-					i = posV2 - 1;
-					while(i >= 0){
-						//sort
-						if(vt2[i]%m > v%m){
-							vt2[i+1] = vt2[i];						
-							i--;
-						}else if(vt2[i]%m < v%m){
-							break;					
-						}else if(abs(vt2[i])%2 == 0 && abs(v)%2 == 1){
-							vt2[i+1] = vt2[i];						
-							i--;
-						}else if(abs(vt2[i])%2 == 0 && abs(v)%2 == 0 && vt2[i] > v){
-							vt2[i+1] = vt2[i];						
-							i--;
-						}else if(abs(vt2[i])%2 == 1 && abs(v)%2 == 1 && vt2[i] < v){
-							vt2[i+1] = vt2[i];						
-							i--;
-						}else{
-							break;
+					posV1++;
+				
+				}else if(v%m > 1){ // modulo maior q 1; //correto
+					if(posV2 == 0){
+						vt2[0] = v;		
+					}else{
+						i = posV2 - 1;
+						while(i >= 0){
+							//sort
+							if(vt2[i]%m > v%m){
+								vt2[i+1] = vt2[i];						
+								i--;
+							}else if(vt2[i]%m < v%m){
+								break;					
+							}else if(abs(vt2[i])%2 == 0 && abs(v)%2 == 1){
+								vt2[i+1] = vt2[i];						
+								i--;
+							}else if(abs(vt2[i])%2 == 0 && abs(v)%2 == 0 && vt2[i] > v){
+								vt2[i+1] = vt2[i];						
+								i--;
+							}else if(abs(vt2[i])%2 == 1 && abs(v)%2 == 1 && vt2[i] < v){
+								vt2[i+1] = vt2[i];						
+								i--;
+							}else{
+								break;
+							}
 						}
+						vt2[i+1] = v;	
 					}
-					vt2[i+1] = v;	
-				}
-				posV2++;
+					posV2++;
 
-			}else if(v%m == -1){ // modulo -1; //correto
-				if(posV01 == 0){
-					vt01[0] = v;		
-				}else{
-					i = posV01 - 1;
-					while(i >= 0){
-						//sort
-						if(abs(vt01[i])%2 == 0 && abs(v)%2 == 1){
-							vt01[i+1] = vt01[i];						
-							i--;
-						}else if(abs(vt01[i])%2 == 0 && abs(v)%2 == 0 && vt01[i] > v){
-							vt01[i+1] = vt01[i];						
-							i--;
-						}else if(abs(vt01[i])%2 == 1 && abs(v)%2 == 1 && vt01[i] < v){
-							vt01[i+1] = vt01[i];						
-							i--;
-						}else{
-							break;
+				}else if(v%m == -1){ // modulo -1; //correto
+					if(posV01 == 0){
+						vt01[0] = v;		
+					}else{
+						i = posV01 - 1;
+						while(i >= 0){
+							//sort
+							if(abs(vt01[i])%2 == 0 && abs(v)%2 == 1){
+								vt01[i+1] = vt01[i];						
+								i--;
+							}else if(abs(vt01[i])%2 == 0 && abs(v)%2 == 0 && vt01[i] > v){
+								vt01[i+1] = vt01[i];						
+								i--;
+							}else if(abs(vt01[i])%2 == 1 && abs(v)%2 == 1 && vt01[i] < v){
+								vt01[i+1] = vt01[i];						
+								i--;
+							}else{
+								break;
+							}
 						}
+						vt01[i+1] = v;	
 					}
-					vt01[i+1] = v;	
-				}
-				posV01++;
-			
-			}else if(v%m < -1){ // modulo menor q -1; //correto
-				if(posV02 == 0){
-					vt02[0] = v;		
-				}else{
-					i = posV02 - 1;
-					while(i >= 0){
-						//sort
-						if(vt02[i]%m > v%m){
-							vt02[i+1] = vt02[i];						
-							i--;
-						}else if(vt02[i]%m < v%m){
-							break;		
-						}else if(vt02[i]%2 == 0 && v%2 == -1){
-							vt02[i+1] = vt02[i];						
-							i--;
-						}else if(vt02[i]%2 == 0 && v%2 == 0 && vt02[i] > v){
-							vt02[i+1] = vt02[i];						
-							i--;
-						}else if(vt02[i]%2 == -1 && v%2 == -1 && vt02[i] < v){
-							vt02[i+1] = vt02[i];						
-							i--;
-						}else{
-							break;
+					posV01++;
+				
+				}else if(v%m < -1){ // modulo menor q -1; //correto
+					if(posV02 == 0){
+						vt02[0] = v;		
+					}else{
+						i = posV02 - 1;
+						while(i >= 0){
+							//sort
+							if(vt02[i]%m > v%m){
+								vt02[i+1] = vt02[i];						
+								i--;
+							}else if(vt02[i]%m < v%m){
+								break;		
+							}else if(vt02[i]%2 == 0 && v%2 == -1){
+								vt02[i+1] = vt02[i];						
+								i--;
+							}else if(vt02[i]%2 == 0 && v%2 == 0 && vt02[i] > v){
+								vt02[i+1] = vt02[i];						
+								i--;
+							}else if(vt02[i]%2 == -1 && v%2 == -1 && vt02[i] < v){
+								vt02[i+1] = vt02[i];						
+								i--;
+							}else{
+								break;
+							}
 						}
+						vt02[i+1] = v;	
 					}
-					vt02[i+1] = v;	
+					posV02++;
+				
 				}
-				posV02++;
-			
+
+			}printf("%d %d\n", n, m);
+			for(i = 0; i < posV02; i++){
+				printf("%i\n", vt02[i]);
 			}
-
-		}printf("%d %d\n", n, m);
-		for(i = 0; i < posV02; i++){
-			printf("%i\n", vt02[i]);
+			for(i = 0; i < posV01; i++){
+				printf("%i\n", vt01[i]);
+			}
+			for(i = 0; i < posV0; i++){
+				printf("%i\n", vt0[i]);
+			}
+			for(i = 0; i < posV1; i++){
+				printf("%i\n", vt1[i]);
+			}
+			for(i = 0; i < posV2; i++){
+				printf("%i\n", vt2[i]);
+			}
 		}
-		for(i = 0; i < posV01; i++){
-			printf("%i\n", vt01[i]);
-		}
-		for(i = 0; i < posV0; i++){
-			printf("%i\n", vt0[i]);
-		}
-		for(i = 0; i < posV1; i++){
-			printf("%i\n", vt1[i]);
-		}
-		for(i = 0; i < posV2; i++){
-			printf("%i\n", vt2[i]);
-		}
-
 	}
 	return 0;
 }
