d46e9dff109b2b26d1023255f27c60c7c6cda175
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-26 00:33:47 +0000

    Create 14.c

---
+++ Lista2/14.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+	int i,n;
+	
+	printf("Digite o valor a ser utilizado!\n");
+	scanf("%d", &n);
+
+	printf("%d - ",n);
+	printf(" %d centenas,", n/100 );
+	printf(" %d dezenas e", (n%100 - n%10)/10);
+	printf(" %d unidades.\n", n%100 - (n%100 - n%10));
+
+	return 0;
+}
