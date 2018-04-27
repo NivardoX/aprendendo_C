1bc2bfdacd55a835de1934b7ddaaf2b00572ac82
Joel <20496128+JoeVictor22@users.noreply.github.com>
2018-04-27 00:24:23 +0000

    Create 02.c

---
+++ Lista2/02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+	char c;
+	int i,sum = 0;
+	
+	printf("Digite um char\n");
+	scanf("%c", &c);	
+
+	for(i = 0; i < (sizeof(char)*8);i++){
+		if((c & 1)%2 == 0){
+			sum++;	
+		}
+		c = c >> 1;
+	}
+
+	printf("O total de bits 1 eh %d\n", sum);
+
+	return 0;
+}
